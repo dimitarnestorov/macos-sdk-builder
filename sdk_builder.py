ec70c5c6e76ca97754138c7fa78909b3297361d5
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-03-30 13:33:09 +0300

    Build macOS SDK

--- sdk_builder.py
+++ sdk_builder.py
@@ -15,33 +15,34 @@ classmap = {}
 working_dir = ".sdkbuilder"
 
 
-def dump(fold, fw_name):
-    fd = open(f'./System/Library/{fold}/{fw_name}.framework/{fw_name}', 'rb')
+def dump(fold, ios_fw, fw_name):
+    fw_path = f'System/iOSSupport/System/Library/{fold}/{fw_name}.framework' if ios_fw else f'System/Library/{fold}/{fw_name}.framework'
+    fd = open(f'./{fw_path}/{fw_name}', 'rb')
     
     library = ktool.load_image(fd)
     objc_lib = ktool.load_objc_metadata(library)
         
-    os.makedirs(f'{working_dir}/System/Library/{fold}/{fw_name}.framework', exist_ok=True)
-    shutil.copyfile(f'./System/Library/{fold}/{fw_name}.framework/{fw_name}', f'{working_dir}/System/Library/{fold}/{fw_name}.framework/{fw_name}')
+    os.makedirs(f'{working_dir}/{fw_path}', exist_ok=True)
+    shutil.copyfile(f'./{fw_path}/{fw_name}', f'{working_dir}/{fw_path}/{fw_name}')
     
     tbd_text = ktool.generate_text_based_stub(library, compatibility=True)
-    with open(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/{fw_name}.tbd', 'w') as tbd_out:
+    with open(f'{working_dir}/{fw_path}/{fw_name}.tbd', 'w') as tbd_out:
         tbd_out.write(tbd_text)
         
-    os.makedirs(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/Headers', exist_ok=True)
+    os.makedirs(f'{working_dir}/{fw_path}/Headers', exist_ok=True)
     
     header_dict = ktool.generate_headers(objc_lib, sort_items=True)
     for header_name in header_dict:
-        with open(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/Headers' + '/' + header_name,
+        with open(f'{working_dir}/{fw_path}/Headers' + '/' + header_name,
                   'w') as out:
             out.write(str(header_dict[header_name]))
 
 
 def trydump(item):
-    fold, fw_name = item
+    fold, ios_fw, fw_name = item
     try:
         print(f'Dumping {fold} ' + fw_name)
-        dump(fold, fw_name.split('.')[0])
+        dump(fold, ios_fw, fw_name.split('.')[0])
     except Exception as ex:
         print(ex)
         print(f'{fw_name} Fail')
@@ -55,7 +56,7 @@ if __name__ == '__main__':
     
     public_frameworks = sorted(public_frameworks)
     executor = concurrent.futures.ProcessPoolExecutor(3)
-    futures = [executor.submit(trydump, ('Frameworks', item)) for item in public_frameworks]
+    futures = [executor.submit(trydump, ('Frameworks', False, item)) for item in public_frameworks]
     concurrent.futures.wait(futures)
 
     privateframeworks = []
@@ -66,5 +67,26 @@ if __name__ == '__main__':
     privateframeworks = sorted(privateframeworks)
 
     executor = concurrent.futures.ProcessPoolExecutor(3)
-    futures = [executor.submit(trydump, ('PrivateFrameworks', item)) for item in privateframeworks]
+    futures = [executor.submit(trydump, ('PrivateFrameworks', False, item)) for item in privateframeworks]
+    concurrent.futures.wait(futures)
+    
+    public_frameworks = []
+
+    for fw in os.listdir('./System/iOSSupport/System/Library/Frameworks/'):
+        public_frameworks.append(fw)
+    
+    public_frameworks = sorted(public_frameworks)
+    executor = concurrent.futures.ProcessPoolExecutor(3)
+    futures = [executor.submit(trydump, ('Frameworks', True, item)) for item in public_frameworks]
+    concurrent.futures.wait(futures)
+
+    privateframeworks = []
+
+    for fw in os.listdir('./System/iOSSupport/System/Library/PrivateFrameworks/'):
+        privateframeworks.append(fw)
+
+    privateframeworks = sorted(privateframeworks)
+
+    executor = concurrent.futures.ProcessPoolExecutor(3)
+    futures = [executor.submit(trydump, ('PrivateFrameworks', True, item)) for item in privateframeworks]
     concurrent.futures.wait(futures)
